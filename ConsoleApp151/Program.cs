a7c37b5547c9ec6b022c6d5fb077a3d64a156f00
ygursu <yasemingrs@gmail.com>
2024-03-22 23:45:21 +0300

    Proje dosyası ekle.

---
+++ ConsoleApp151/Program.cs
@@ -0,0 +1,18 @@
+﻿using System;
+class HelloWorld
+{
+    static void Main()
+    {
+
+        int sayi, ters = 0;
+        Console.WriteLine("Bir sayı giriniz : ");
+        sayi = int.Parse(Console.ReadLine());
+        while (sayi != 0)
+        {
+            ters = ters * 10;
+            ters = ters + sayi % 10;
+            sayi = sayi / 10;
+        }
+        Console.WriteLine("Sayının tersi : " + ters);
+    }
+}
